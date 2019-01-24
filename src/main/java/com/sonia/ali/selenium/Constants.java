371b5f943da894d6a3af0d52852a2ddf8dcad191
SoniaAli05 <45198070+SoniaAli05@users.noreply.github.com>
2019-01-24 14:23:26 +0000

    Update Constants.java

--- src/main/java/com/sonia/ali/selenium/Constants.java
+++ src/main/java/com/sonia/ali/selenium/Constants.java
@@ -4,7 +4,4 @@ public class Constants {
 
 	public static final String websiteURL = "https://www.phptravels.net/en";
 
-	//public static final String addUser = "http://thedemosite.co.uk/addauser.php";
-
-	//public static final String login = "http://thedemosite.co.uk/login.php";
 }
